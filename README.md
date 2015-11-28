02f9ad557b3c007303a8fbee19f145d76c8aae65
chreman <web@christopherkittel.eu>
2015-11-28 17:47:42 +0100

    changed framework to GET/POST

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # agroscraper
 Getting structured data out of transparenzdatenbank, which shows EU aids for Austrian agricultural businesses.
+
+
+
+## data model
+
+
+[
+	{
+		"id": int,
+		"recipient": "str",
+		"zip code": int,
+		"municipality": "str",
+		"year": int,
+		"total_amount": float
+		"details": [
+			{
+				"id": int,
+				"type": "str",
+				"description": "str",
+				"partial_amount": float
+			}
+		]	
+	}
+]
--- source/agroscraper.py
+++ source/agroscraper.py
@@ -5,78 +5,95 @@
 Extracting information from transparenzdatenbank.at
 
 Output from the #gutedaten Hackathon in Graz, Sat. 28. Nov. 2015
+
+
+
+[
+    {
+        "id": int,
+        "recipient": "str",
+        "zip code": int,
+        "municipality": "str",
+        "year": int,
+        "total_amount": float
+        "details": [
+            {
+                "id": int,
+                "type": "str",
+                "description": "str",
+                "partial_amount": float
+            }
+        ]   
+    }
+]
 """
-
-
-# import headless browsing
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait # available since 2.4.0
-from selenium.webdriver.support import expected_conditions as EC # available since 2.26.0
-
-
-# import data handling
-from bs4 import BeautifulSoup
 
 
 __author__ = "Christopher Kittel"
 __copyright__ = "Copyright 2015"
 __license__ = "MIT"
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 __maintainer__ = "Christopher Kittel"
 __email__ = "web@christopherkittel.eu"
 __status__ = "Prototype" # 'Development', 'Production' or 'Prototype'
 
 
-# create webdriver
-ff = webdriver.Firefox()
-ff.get("http://transparenzdatenbank.at")
+import requests
+import time
 
-# wait until loaded completely
-try:
-    searchbutton = WebDriverWait(ff, 10).until(EC.presence_of_element_located((By.ID, "startSearch")))
-finally:
-    ff.find_element_by_xpath("//button[@id='startSearch']").click()
+import json
 
 
-# increase paginatoin
-options = ff.find_element_by_xpath("//select[@id='paginationPerPage']")
-for option in options.find_elements_by_tag_name("option"):
-    # select option '100'
-    if option.text == "100":
-        option.click()
+url = 'http://transparenzdatenbank.at/suche'
+
+header = {"Accept": "application/json, text/plain, */*",
+           "Accept-Encoding": "gzip, deflate",
+           "Accept-Language": "de,en-US;q=0.7,en;q=0.3",
+           "Content-Length": "100",
+           "Content-Type": "application/json;charset=utf-8",
+           "DNT": "1",
+           "Host": "transparenzdatenbank.at",
+           "PAGINATION_CURRENT": "1",
+           "PAGINATION_PER_PAGE": "140000",
+           "Referer": "http://transparenzdatenbank.at/",
+           "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:42.0) Gecko/20100101 Firefox/42.0"
+           }
+
+payload = "{\"name\":\"\", \"betrag_von\":\"\", \"betrag_bis\":\"\", \"gemeinde\":\"\", \"massnahme\":null,\"jahr\":2014, \"sort\":\"name\"}"
+
+
+raw = []
+response = requests.request("POST", url, data=payload, headers = header)
+raw.extend(response.json())
+
+# make a quick save
+with open("firstround.json", "w") as outfile:
+    json.dump(response.json(), outfile)
+
+
+ids = [r.get("id") for r in raw]
+
+
+# create new header for detailed search
+
+searchheader = {'Host': 'transparenzdatenbank.at',
+                 'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:42.0) Gecko/20100101 Firefox/42.0',
+                 'Accept': 'application/json, text/plain, */*',
+                 'Accept-Language': 'de,en-US;q=0.7,en;q=0.3',
+                 'Accept-Encoding': 'gzip, deflate',
+                 'DNT': '1',
+                 'Referer': 'http://transparenzdatenbank.at/',
+                 'Cookie': 'AMA=!nuCdkyly1f7Zu8TJc9WWi6/Juy2r0rwBzfMkAVDZdlK7ppJA07eM8ERA7mL9kNBeY+AJZtNYoza5Ivk=; TS018cbf1b=015c95df5d1c780212c177d6031479ab2c968358d311dc333c0baee3ece7918812734b06fc4e15c151ca4dfe6a81a3b37c01e323c9'
+                 }
+
+
+details = []
+
+for id_ in ids[:10]:
+    searchurl = 'http://transparenzdatenbank.at/suche/details/%s/2014' %id_
+    detail = requests.get(searchurl, searchheader)
+    details.extend(detail.json())
 
 
 
-# MAKE A FUNCTION, HAS TO BE LOOPABLE FOR PAGINATION
-
-# overall table
-table = ff.find_element_by_xpath("//table[@id='result']")
-
-
-elements = table.find_elements_by_xpath("//tbody[@class='ng-scope']")
-
-for element in elements[:3]:
-    element.find_element_by_xpath("a[@data-ng-click=['showDetail(record);$event.stopPropagation();']").click()
-    records = element.find_elements_by_xpath("tr[@data-ng-click='showDetail(record)']")
-    for record in records:
-        metadata = record.find_elements_by_xpath("td[@class='ng-binding']")
-        overall_sum = record.find_element_by_xpath("td[@class='sum ng-binding']")
-        
-        for md in metadata:
-            print md.text
-        print overall_sum.text
-
-        details = record.find_elements_by_xpath("tr[@data-ng-show='record.show']")
-        for detail in details:
-            title = detail.find_elements_by_tag_name("h4")
-            descriptions = detail.find_elements_by_xpath("div[@class='sum ng-binding']/p")
-            sums = detail.find_elements_by_xpath("div[@class='sum ng-binding']/p")
-            rows= zip(descriptions, sums)
-            for row in rows:
-                d, s = row
-                print d.text, s.text
-
-
-
-# MAKE A CLASS FOR INDIVIDUAL SEARCHRESULTS WHICH CAN BE EXPORTED AS CSV
+results = {}
