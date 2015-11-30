2ef7a7aa71774c69bb81be7f2cba0154889da5f5
chreman <web@christopherkittel.eu>
2015-11-30 11:02:35 +0100

    improved datamodel to ensure uniqueness

--- source/agroscraper.py
+++ source/agroscraper.py
@@ -7,8 +7,7 @@ Extracting information from transparenzdatenbank.at
 Output from the #gutedaten Hackathon in Graz, Sat. 28. Nov. 2015
 
 
-
-[
+{id:
     {
         "id": int,
         "recipient": "str",
@@ -25,7 +24,7 @@ Output from the #gutedaten Hackathon in Graz, Sat. 28. Nov. 2015
             }
         ]   
     }
-]
+}
 """
 
 
@@ -83,8 +82,7 @@ searchheader = {'Host': 'transparenzdatenbank.at',
                  'Accept-Language': 'de,en-US;q=0.7,en;q=0.3',
                  'Accept-Encoding': 'gzip, deflate',
                  'DNT': '1',
-                 'Referer': 'http://transparenzdatenbank.at/',
-                 'Cookie': 'AMA=!nuCdkyly1f7Zu8TJc9WWi6/Juy2r0rwBzfMkAVDZdlK7ppJA07eM8ERA7mL9kNBeY+AJZtNYoza5Ivk=; TS018cbf1b=015c95df5d1c780212c177d6031479ab2c968358d311dc333c0baee3ece7918812734b06fc4e15c151ca4dfe6a81a3b37c01e323c9'
+                 'Referer': 'http://transparenzdatenbank.at/'
                  }
 
 
@@ -112,7 +110,7 @@ def get_missing_ids(raw, results):
     returns missing ones.
     """
     all_ids = set(raw.keys())
-    cached_ids = set([r.get("id") for r in results])
+    cached_ids = set(results.keys())
     print("There are {0} IDs in the dataset, we already have {1}. {2} are missing.".format(len(all_ids), len(cached_ids), len(all_ids) - len(cached_ids)))
     return all_ids - cached_ids
 
@@ -132,11 +130,12 @@ def crawl(raw, results, missing_ids):
     progress = 0.0
     maxi = len(missing_ids)
 
+    i = 0
     for id_ in missing_ids:
 
         try:
             r = raw.get(id_)
-            results.append(enhance_raw(r, id_))
+            results[id_] = enhance_raw(r)
 
             # more progress
             progress += 1
@@ -146,12 +145,22 @@ def crawl(raw, results, missing_ids):
             # store cache
             with open("agrofunding.json", "w") as outfile:
                 json.dump(results, outfile)
+
+        i += 1
+        if i > 1000:
+            with open("agrofunding.json", "w") as outfile:
+                json.dump(results, outfile)
+            i = 0
+
     return results
 
-def enhance_raw(r, missing_id):
+def enhance_raw(r):
     """
     Reformats the raw data.
     Adds the detailed information to a raw search result.
+
+    args: r = {raw_search_result}
+
     """
     result = {}
     result["id"] = int(r.get("id"))
@@ -191,22 +200,22 @@ def save2csv(results, filename):
     with open(filename+".csv", "w") as csvfile:
         agrowriter = csv.writer(csvfile, delimiter = ",", quotechar='"')
         agrowriter.writerow(["id", "recipient", "year", "postcode", "municipality", "total_amount", "detail_id", "type", "partial_amount"])
-        for result in results:
+        for result in results.values():
             metadata = [result.get("id"), result.get("recipient"), result.get("year"), result.get("postcode"), result.get("municipality"), result.get("total_amount")]
             for detail in result.get("details"):
                 data = [detail.get("id"), detail.get("type"), detail.get("partial_amount")]
                 agrowriter.writerow(metadata+data)
 
 def main():
-    raw, results = get_cache()
-    missing_ids = get_missing_ids(raw, results)
-    results = crawl(raw, results, missing_ids)
+    raw, old_results = get_cache()
+    missing_ids = get_missing_ids(raw, old_results)
+    new_results = crawl(raw, old_results, missing_ids)
 
     # store final
     with open("agrofunding.json", "w") as outfile:
-        json.dump(results, outfile)
+        json.dump(new_results, outfile)
 
-    save2csv(results, "agrofunding")
+    save2csv(new_results, "agrofunding")
 
 
 if __name__ == '__main__':
